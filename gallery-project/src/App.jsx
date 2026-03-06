9674179832dfb484553b31764b4abc6c1e3b8986
Bishwash Dhital <bishwashdhital73@gmail.com>
2026-03-06 14:03:56 +0545

    create galleryy project

---
+++ gallery-project/src/App.jsx
@@ -0,0 +1,109 @@
+// import React from 'react'
+
+// const App = () => {
+//   const user={
+//     username:'Sarthak',
+//     age:18,
+//     city:"kathmandu"
+//   }
+//   localStorage.setItem('user',JSON.stringify(user))
+//   const userea=JSON.parse(localStorage.getItem('user'))
+//   console.log(userea)
+//   return (
+//     <div>App</div>
+//   )
+// }
+
+// export default App
+
+
+// import React, { useEffect, useState } from 'react'
+
+// const App = () => {
+//   const [num, setNum] = useState('')
+//   function random(){
+//     console.log('hello')
+//   }
+//   useEffect(()=>{
+//     console.log('use effect is running.....')
+//   },[num])
+//   return (
+//     <div>
+//       <h1>{num}</h1>
+//       <button onClick={()=>{
+//         setNum(num+1)
+//       }} >click</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Card from './componesnts/Card'
+
+const App = () => {
+
+  const [userData, setUserData] = useState([]);
+
+  const [index, setIndex] = useState(1)
+
+  const getData = async () => {
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=10`)
+    setUserData(response.data)
+  }
+
+  useEffect(function () {
+    getData()
+  }, [index])
+
+
+  let printUserData = <h3 className='text-gray-300 text-xs absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold'>Loading...</h3>
+
+  if (userData.length > 0) {
+    printUserData = userData.map(function (elem, idx) {
+
+      return <div key={idx}>
+        <Card elem={elem} />
+      </div>
+    })
+  }
+
+  return (
+    <div className='bg-black overflow-auto h-screen p-4 text-white'>
+      <div className='flex h-[82%] flex-wrap gap-4 p-2'>
+        {printUserData}
+      </div>
+
+      <div className='flex justify-center gap-6 items-center p-4'>
+        <button
+          style={{ opacity: index == 1 ? 0.6 : 1 }}
+          className='bg-amber-400 text-sm cursor-pointer active:scale-95 text-black rounded px-4 py-2 font-semibold'
+          onClick={() => {
+            if (index > 1) {
+              setIndex(index - 1)
+              setUserData([])
+            }
+          }}
+        >
+          Prev
+        </button>
+        <h4>Page {index}</h4>
+        <button
+          className='bg-amber-400 text-sm cursor-pointer active:scale-95 text-black rounded px-4 py-2 font-semibold'
+          onClick={() => {
+            setUserData([])
+
+            setIndex(index + 1)
+
+          }}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
